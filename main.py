29edc6be73dfaf559e699e10772fca9da6ded397
Shivam <Tanejashivam.st@gmail.com>
2018-11-11 09:24:23 -0500

    removeSlave is incomplete

--- main.py
+++ main.py
@@ -1,90 +1,82 @@
 import requests
+import time
 import xml.etree.ElementTree as ET
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 
-@app.route("/")
-def hello():
-    return "Hello World"
-
-@app.route('/')
-def my_form():
-    return render_template('input.html')
-
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     return processed_text
 
-@app.route('/volume')
-def volume():
-    url = "http://192.168.1.17:8090/volume"
-    r = requests.get(url)
-    data = (r.text)
-    print (data)
-    return data
+# @app.route('/volume')
+# def volume():
+#     url = "http://192.168.1.17:8090/volume"
+#     r = requests.get(url)
+#     data = (r.text)
+#     print (data)
+#     return data
 
-def info():
-    url = "http://192.168.1.17:8090/volume"
-    data = requests.get(url)
-    tree =ET.parse(data)
-    root = tree.getroot()
-    attr = root.attrib
-    splitAttr = atrr.split('=')
-    deviceINFO = splitAttr[1]
-    return deviceINFO
+# def info():
+#     url = "http://192.168.1.17:8090/volume"
+#     data = requests.get(url)
+#     tree = ET.parse(data)
+#     root = tree.getroot()
+#     attr = root.attrib
+#     splitAttr = atrr.split('=')
+#     deviceINFO = splitAttr[1]
+#     return deviceINFO
 
-@app.route('/volumeChange')
-def volumeChange():
-    url = "http://192.168.1.17:8090/volume"
-    data = '<volume>'+my_form_post+'</volume>'
+@app.route('/<ipconfig>/volume/<v>/')
+def volumeChange(ipconfig,v):
+    url = 'http://' + ipconfig + ':8090/volume'
+    r1 = requests.get(url)
+    data1 = (r1.text)
+    print (data1)
+    data = '<volume>' + v + '</volume>'
     r = requests.post(url = url, data = data )
     result = (r.text)
-    return result
+    return data1
 
 
-@app.route('/')
-def getSlaveStatus():
-    url = "http://192.168.1.17:8090/volumeChange"
-    data = requests.get(url)
-    tree =ET.parse(data)
-    root = tree.getroot()
-    attr = root.attrib
-    splitAttr = atrr.split('=')
-    deviceINFO = splitAttr[1]
-    return deviceINFO
+# @app.route('/<ipconfig>/')
+# def getSlaveStatus(ipconfig):
+#     url = 'http://' + ipconfig + ':8090/volume'
+#     data = requests.get(url)
+#     tree =ET.parse(data)
+#     root = tree.getroot()
+#     attr = root.attrib
+#     splitAttr = atrr.split('=')
+#     deviceINFO = splitAttr[1]
+#     return deviceINFO
 
-@app.route('/removeSlave')
-def removeSlave():
-    url = "http://192.168.1.17:8090/removeZoneSlave"
-    if (getSlaveStatus()!= 0):
-        data = '<zone master=' + info() + '><member ipaddress=' getSlaveStatus()' + '>+ info() + '</member></zone>'
-    r = requests.post(url = url, data = data )
-    return r
 
-@app.route('/speaker')
-def lowSpeaker():
-    url = "http://192.168.1.17:8090/speaker"
-    data = "<play_info><app_key>your_app_key_here</app_key><url>https://raw.githubusercontent.com/TANEJS4/BostonHack/master/LowFrequency.mp3</url><service>service text</service><reason>reason text</reason><message>message text</message><volume>40</volume></play_info>"
+# @app.route('/removeSlave')
+# def removeSlave():
+#     url = "http://192.168.1.17:8090/removeZoneSlave"
+#     if (getSlaveStatus()!= 0):
+#         data = '<zone master=' + info() + '><member ipaddress=' getSlaveStatus()' + '>+ info() + '</member></zone>'
+#     r = requests.post(url = url, data = data )
+#     return r
+
+@app.route('/<ipconfig>/speaker')
+def lowSpeaker(ipconfig):
+    url = 'http://' + ipconfig + ':8090/speaker'
+    data = "<play_info><app_key>yMFGZBuUj8II9kMfFN8uw3aWfqczkzed</app_key><url>https://raw.githubusercontent.com/TANEJS4/BostonHack/master/LowFrequency.mp3</url><service>service text</service><reason>reason text</reason><message>message text</message><volume>40</volume></play_info>"
     r = requests.post(url = url, data = data)
     return r
 
 
-@app.route('/speaker')
-def highSpeaker():
-    url = "http://192.168.1.17:8090/speaker"
-    data = "<play_info><app_key>your_app_key_here</app_key><url>https://raw.githubusercontent.com/TANEJS4/BostonHack/master/highFrequency.mp3</url><service>service text</service><reason>reason text</reason><message>message text</message><volume>40</volume></play_info>"
+@app.route('/<ipconfig>/speaker')
+def highSpeaker(ipconfig):
+    rl = 'http://' + ipconfig + ':8090/speaker'
+    data = "<play_info><app_key>yMFGZBuUj8II9kMfFN8uw3aWfqczkzed</app_key><url>https://raw.githubusercontent.com/TANEJS4/BostonHack/master/highFrequency.mp3</url><service>service text</service><reason>reason text</reason><message>message text</message><volume>40</volume></play_info>"
     r = requests.post(url = url, data = data)
     return r
 
 
 
 if __name__ == '__main__':
-    print("Please connect your phone to your speaker via wifi")
-    print("Your IP address can be found by:" +
-            "Going into to your BOSE SoundTouch app -> Tap hamburger menu  -> Select settings -> choose your BOSE device -> get the IP Address ")
-
-    iP = input("Please enter your speaker's IP address")
     app.run(debug=True)
