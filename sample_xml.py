c81a0a1f42348c94ea4022c1d36b1393bd226f19
Shivam <Tanejashivam.st@gmail.com>
2018-11-11 06:58:08 -0500

    removed redundant files

--- sample_xml.py
+++
@@ -1,9 +0,0 @@
-import xmltodict
-
-filename = ("sample.xml");
-
-file1 = open(filename, 'r');
-
-dict_resp = xmltodict(xml,file);
-
-print(dict_resp);
--- start.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-test =  requests.get("https://tanejs4.github.io")
-
-print(type.__getstate__)
