23f29d76de5491745bcb51efe95255b4bdac19d2
Shivam <Tanejashivam.st@gmail.com>
2018-11-11 06:51:36 -0500

    trial with speaker module

--- trial.py
+++ trial.py
@@ -9,27 +9,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World"
 
-@app.route("/")
-def homepage():
-    """
-    we want to input from user : IPADDress
-    give prompt how he can find on the app
-    1. connect your via wifi
-    2. find iP THROUGH phone hamburger -> settinggs -> device -> IP address
-
-     valid -> true
-                 get target volume
-            -> false
-                try again
-    store value => ipconfig:
-        homepage.ipconfig()
-    check if there is a zone
-             -> True
-             call removeSlave
-             else continue
-    """
-
-
 @app.route('/')
 def my_form():
     return render_template('input.html')
@@ -63,9 +42,8 @@ def volumeChange():
     url = "http://192.168.1.17:8090/volume"
     data = '<?xml version="1.0" encoding="UTF-8" ?><volume deviceID="' + info() + '><targetvolume>' + my_form_post + '</targetvolume><actualvolume>' + my_form_post +'</actualvolume><muteenabled>false</muteenabled></volume>'
     r = requests.post(url = url, data = data )
-    data = (r.text)
-    print (data)
-    return data
+    result = (r.text)
+    return result
 
 
 @app.route('/')
@@ -81,10 +59,16 @@ def getSlaveStatus():
 
 @app.route('/removeSlave')
 def removeSlave():
-
-
-
-
+    url = "http://192.168.1.17:8090/removeZoneSlave"
+    if (getSlaveStatus()!= 0):
+        data = '<zone master=' + info() + '><member ipaddress=' getSlaveStatus()' + '>+ info() + '</member></zone>'
+    r = requests.post(url = url, data = data )
+    return r
 
 if __name__ == '__main__':
+    print("Please connect your phone to your speaker via wifi")
+    print("Your IP address can be found by:" +
+            "Going into to your BOSE SoundTouch app -> Tap hamburger menu  -> Select settings -> choose your BOSE device -> get the IP Address ")
+
+    iP = input("Please enter your speaker's IP address")
     app.run(debug=True)
